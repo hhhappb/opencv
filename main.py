3ad2dfb0df247937c49cbc1bbb1fd5c885b42f16
hhhappb <133570902+hhhappb@users.noreply.github.com>
2023-06-29 13:14:55 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,13 @@
 import cv2
-import numpy as np
+import pytesseract
 
-
-def find_circles(image_path):
-    # 读取图像
-    image = cv2.imread('5.png')
-    # 转换为灰度图像
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # 高斯模糊处理
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-
-    # 使用霍夫圆变换检测圆
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, 20,
-                               param1=50, param2=30, minRadius=0, maxRadius=0)
-
-    if circles is not None:
-        # 将圆心坐标取整并转换为整数
-        circles = np.round(circles[0, :]).astype("int")
-
-        # 遍历检测到的圆
-        for (x, y, r) in circles:
-            # 绘制圆和圆心
-            cv2.circle(image, (x, y), r, (0, 255, 0), 4)
-            cv2.circle(image, (x, y), 2, (0, 0, 255), 3)
-
-            # 输出圆心坐标
-            print("圆心坐标：", (x, y))
-
-        # 显示绘制圆后的图像
-        cv2.imshow("Circles", image)
-        cv2.waitKey(0)
-    else:
-        print("未找到圆")
-
-    cv2.destroyAllWindows()
-
-
-# 调用函数并传入图像路径
-find_circles("5.png")
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+img = cv2.imread('1.png')
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+#字符的识别
+text = pytesseract.image_to_string(img,lang='chi_sim')
+#字符的标定
+boxes = pytesseract.image_to_boxes(img,lang='chi_sim')
+print(boxes)
+cv2.imshow("text",img)
+cv2.waitkey(0)
